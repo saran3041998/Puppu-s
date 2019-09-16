532dce662ce07ca4aa06a33ee43a081632f80f76
saran3041998 <46961110+saran3041998@users.noreply.github.com>
2019-09-16 12:26:10 +0530

    Update ConnectionHandler.java

--- SaranyaPup/src/com/cognizant/puppus/dao/ConnectionHandler.java
+++ SaranyaPup/src/com/cognizant/puppus/dao/ConnectionHandler.java
@@ -8,7 +8,7 @@ public class ConnectionHandler {
 	public static Connection getConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/truyum", "root", "root");
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/puppus", "root", "root");
 		} catch (SQLException | ClassNotFoundException ex) {
 			throw new RuntimeException("Error connecting to the database", ex);
 		}
